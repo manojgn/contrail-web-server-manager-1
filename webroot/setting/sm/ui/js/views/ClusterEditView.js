4e38f88a540dc32c14aeef2e4b17be236d14fa39
Chinmay Zanpure <czanpure@juniper.net>
2015-02-17 13:18:34 -0800

    Increased height of roles multiselect in Assign Roles for cluster

--- webroot/setting/sm/ui/js/views/ClusterEditView.js
+++ webroot/setting/sm/ui/js/views/ClusterEditView.js
@@ -908,7 +908,7 @@ define([
                                 placeholder: smwl.SEARCH_ROLES
                             },
                             minWidth: 150,
-                            height: 200,
+                            height: 350,
                             selectedList: 2,
                             data: [
                                 {
